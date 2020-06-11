d38ae0559242e91f23d83cb106b0ad23dbf510ea
Micah Sherr <msherr@cs.georgetown.edu>
2020-06-11 13:54:45 -0400

    some good fixes

--- pt-proxy.py
+++ pt-proxy.py
@@ -28,6 +28,12 @@ def parse_args():
     group.add_argument('-s', '--server', dest='servermode', action='store_false')
     
     parser.add_argument(
+        '-l', '--logfile',
+        dest="logfile",
+        help="log file",
+        required = True
+        )
+    parser.add_argument(
         '-b', '--binary',
         dest="ptbinary",
         help="path to PT proxy (e.g., /usr/bin/obfs4proxy)",
@@ -40,13 +46,24 @@ def parse_args():
         default='obfs4'
         )
     parser.add_argument(
-        '-l', '--logfile',
-        dest="logfile",
-        help="log file",
-        required = True
+        '-B', '--bridge',
+        dest='bridge',
+        help='IP and port of a bridge (e.g., 1.2.3.4:443)',
+        )
+    parser.add_argument(
+        '-i', '--info',
+        dest='bridgeinfo',
+        help='bridge-specific information (e.g., cert=ssH+9rP8dG2NLDN2XuFw63hIO/9MNNinLmxQDpVa+7kTOa9/m+tGWT1SmSYpQ9uTBGa6Hw;iat-mode=0).  You should probably escape this argument.',
+        required=True
         )
 
+
     args = parser.parse_args()
+
+    if args.clientmode is True and args.bridge is None:
+        print( 'client mode requires bridge (--bridge) to be specified' )
+        exit(1)
+
     return args
 
 
@@ -70,9 +87,11 @@ used to communicate with the other endpoint
 def launch_pt_binary( args ):
     global proc
     logger = logging.getLogger('pt-proxy-client')        
-    logger.info( 'launch PT client' )
 
-    tmpdir = "/tmp/micah" #tempfile.mkdtemp()
+    (bridge_ip,bridge_port) = args.bridge.split(':')
+
+    logger.info( 'launch PT client' )
+    tmpdir = tempfile.mkdtemp()
     logger.info( 'PT will keep state in %s', tmpdir )
     
     os.environ['TOR_PT_MANAGED_TRANSPORT_VER'] = '1'
@@ -115,16 +134,24 @@ def launch_pt_binary( args ):
         
         s = socks.socksocket()
         try:
-            # authenticate to it
-            s.set_proxy(socks.SOCKS5, addr, port, username='cert=ssH+9rP8dG2NLDN2XuFw63hIO/9MNNinLmxQDpVa+7kTOa9/m+tGWT1SmSYpQ9uTBGa6Hw;iat-mode=0',password='\0')
-#            s.set_proxy(socks.SOCKS5, addr, port, username='cert=sjVM7v2cpvtw4GLWaP+TEVUeEhld07iGa8AqEYQk3IHIbtr0Rpiqw6weoKMcnZEZ1+pmFQ')
-            s.connect(("209.148.46.65", 443))   # TODO: DONT HARDCODE THIS
+            # authenticate to PT bridge
+            s.set_proxy(socks.SOCKS5, addr, port, username=args.bridgeinfo, password='\0')
+            logger.info( 'authenticated to PT bridge' )
+            logger.info( 'connecting to bridge (%s,%s)' % (bridge_ip,bridge_port) )
+            s.connect((bridge_ip, int(bridge_port))) 
         except socks.ProxyConnectionError as e:
             logger.error( 'cannot connect to proxy: %s' % e )
             time.sleep(200)
             proc.kill()
             return None
+        except socks.GeneralProxyError as e:
+            logger.error( 'cannot connect to proxy: %s' % e )
+            time.sleep(200)
+            proc.kill()
+            return None
 
+        logger.info( 'connected to bridge (%s,%s)' % (bridge_ip,bridge_port) )
+        
         return s                          # all's good
         
     except FileNotFoundError as e:
@@ -189,6 +216,8 @@ def main( args ):
     if pt_sock != None:
         if args.clientmode is True:
             launch_client_listener_service( pt_sock )
+        elif args.servermode is True:
+            logger.warn( 'server mode is not yet implemented' )   # TODO
 
     proc.kill()
     exit( 0 )                             # all's well that ends well
